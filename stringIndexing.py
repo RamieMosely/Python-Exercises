dab5762e9afe6855e4987a53e50f3e968add0c2b
Ramie Mosely <52608883+RamieMosely@users.noreply.github.com>
2024-10-19 02:33:47 -0400

    created email slicing program to practice string indexing

---
+++ stringIndexing.py
@@ -0,0 +1,7 @@
+#Indexing = accessing elements of a sequence using [] (indexing operator)
+#[start : end : step]
+#You can use negative indexing to reverse a string
+
+userInput = input("Please enter your credit card number:\n")
+
+print(userInput[0:5:2])
